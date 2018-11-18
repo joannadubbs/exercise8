ce9387d0c8884694ab5644a2aaa8dfba513fcac3
Joanna Williams <joannalynwilliams@gmail.com>
2018-11-18 17:25:03 -0500

    Url 2 at least works

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@ $(function(){
   console.log('scripts loaded');
 
   var url = 'http://api.open-notify.org/iss-now.json';
+  var url2 = '';
   var html = '';
   var data = '';
   var data2 = '';
   var coordinates = [];
   var country = [];
+  //var position = {
+  //  latitude: data.iss_position;
+    //longitude: ;
+//  }
 
 
 // Get coordinates to show up
@@ -20,13 +25,16 @@ $(function(){
     success: function(data){
     console.log(data.iss_position);
     coordinates = data.iss_position;
+    console.log(coordinates.latitude);
+    console.log(coordinates.longitude);
+    url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + coordinates.longitude + '&lon' + coordinates.latitude;
 
     (function(coordinates){
         console.log(data.iss_position);
         html += '<div class="coordinates-location flex">';
           html += '<div class="text">';
           html += '<a href="' + coordinates.url + '" target ="_blank">';
-          html += '<h1 class="coordinates">text here ' + data.iss_position + '</em></h1>';
+          html += '<h1 class="coordinates">The space station is currently at.. ' + data.iss_position + '</em></h1>';
           html += '</a></div>';
         html += '</div>';
       });
@@ -35,16 +43,13 @@ $(function(){
 
     } //success
 
+    //Declare URL for second ajax call
+
   }); //closes coordinates ajax request
-
-  var latitude = url.iss_position.latitude;
-  var longitude = url.iss_position.longitude;
-
-  var url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + latitude + '&lon' + longitude;
 
   $.ajax({
     type:'GET',
-    url:url2 + latitude + '&lon' + longitude,
+    url:url2,
     dataType: 'json',
     async: true,
     data: data,
@@ -68,6 +73,8 @@ $(function(){
 
   }); //closes ajax request
 
+  //Get function to refresh every 5 seconds
 
+  //display error message
 
 });
