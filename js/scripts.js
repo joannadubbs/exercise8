89591790a33f6e8ceb9652ffe2378128136ab442
Joanna Williams <joannalynwilliams@gmail.com>
2018-11-18 19:27:29 -0500

    Both coordinates and location showing up in console

--- js/scripts.js
+++ js/scripts.js
@@ -27,51 +27,54 @@ $(function(){
     coordinates = data.iss_position;
     console.log(coordinates.latitude);
     console.log(coordinates.longitude);
-    url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + coordinates.longitude + '&lon' + coordinates.latitude;
+    url2 = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=' + coordinates.latitude + '&lon=' + coordinates.longitude;
+    console.log(url2);
 
     (function(coordinates){
         console.log(data.iss_position);
         html += '<div class="coordinates-location flex">';
           html += '<div class="text">';
           html += '<a href="' + coordinates.url + '" target ="_blank">';
-          html += '<h1 class="coordinates">The space station is currently at.. ' + data.iss_position + '</em></h1>';
+          html += '<h1 class="coordinates-header">The space station is currently at.. ' + data.iss_position + '</em></h1>';
           html += '</a></div>';
         html += '</div>';
       });
 
       $('#results').html(html);
 
-    } //success
+      $.ajax({ //on success call second ajax
+        type:'GET',
+        url:url2,
+        dataType: 'json',
+        async: true,
+        data: data,
+        success: function(data2){
+        place = data2.country;
+        console.log(data2.address.country);
+
+        (function(place){
+            console.log(data2.address.country);
+            html += '<div class="coordinates-location flex">';
+              html += '<div class="text">';
+              html += '<a href="' + place.url2 + '" target ="_blank">';
+              html += '<h2 class="place-header">text here ' + data2.address.country + '</em></h2>';
+              html += '</a></div>';
+            html += '</div>';
+          });
+
+          $('#results').html(html);
+
+        } //success
+
+      }); //closes ajax request
+
+    } //success of first ajax call
 
     //Declare URL for second ajax call
 
   }); //closes coordinates ajax request
 
-  $.ajax({
-    type:'GET',
-    url:url2,
-    dataType: 'json',
-    async: true,
-    data: data,
-    success: function(data2){
-    console.log(data2.address.country);
-    place = data2.country;
 
-    (function(place){
-        console.log(data2.address.country);
-        html += '<div class="coordinates-location flex">';
-          html += '<div class="text">';
-          html += '<a href="' + place.url2 + '" target ="_blank">';
-          html += '<h1 class="byline">text here ' + data2.address.country + '</em></h1>';
-          html += '</a></div>';
-        html += '</div>';
-      });
-
-      $('#results').html(html);
-
-    } //success
-
-  }); //closes ajax request
 
   //Get function to refresh every 5 seconds
 
